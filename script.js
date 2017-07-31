69ef553de4a272c4d3cf48561e48526b8f317502
Louis Gabrie <louisgabrie1450@gmail.com>
2017-07-31 15:39:49 +0000

    I added things

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 // JavaScript File
 // JavaScript File
-// $( document ).ready(function() {
-//     $("#photography").hover(function() { 
-//      $("#text_container").append("<p>" + "I have a passion for photography and I usually shoot photos on my iphone when im up and about when im in NY. I think that every photo has a story behind it, and through that photo people are allowed to express their thoughts or/and feelings." + "</p>");
-//      $("#text_container").append("<p>" + "If you'll like to see some photos that I produce, then make sure to click the button in order to see my page." + "</p>");
-     
-// });
-//   });
-  
-//   $("#skateboarding").hover(function() { 
-//       $("#text_container2").append("<p>" + "I have a passion for photography and I usually shoot photos on my iphone when im up and about when im in NY. I think that every photo has a story behind it, and through that photo people are allowed to express their thoughts or/and feelings." + "</p>");
-//      $("#text_container2").append("<p>" + "If you'll like to see some photos that I produce, then make sure to click the button in order to see my page." + "</p>");
-// });
-//   });
-  
+$( document ).ready(function() {
+ 
+ 
+});
   
   
